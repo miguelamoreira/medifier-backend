f2782ab66a719096c906c5f304ee1c163e8ff5cc
miguelamoreira <40210256@esmad.ipp.pt>
2024-10-28 18:35:45 +0000

    publishers, subscribers

--- publishers/publishers.js
+++ publishers/publishers.js
@@ -1,2 +1,31 @@
-const { publish } = require('../services/mqttService');
+const mqttService = require('../services/mqttService');
+
+function publishTouchState(state) {
+    const message = JSON.stringify({
+        sensor: 'touch',
+        state: state
+    });
+
+    mqttService.publish('Estado sensores', message);
+}
+
+function publishWeightState(state) {
+    const message = JSON.stringify({
+        sensor: 'weight',
+        state: state
+    });
+
+    mqttService.publish('Estado sensores', message);
+}
+
+function publishUpdate(item) {
+    const message = JSON.stringify(item);
+    mqttService.publish('Agenda', message);
+}
+
+module.exports = {
+    publishTouchState,
+    publishWeightState,
+    publishUpdate
+}
 
--- subscribers/subscribers.js
+++ subscribers/subscribers.js
@@ -1,1 +1,23 @@
-const { subscribe } = require('../services/mqttService');
+const mqttService = require('../services/mqttService');
+
+mqttService.subscribe('Estado sensores');
+mqttService.subscribe('Agenda');
+
+function processSensorState(data) {
+    const { sensor, state } = data;
+    console.log(`Estado do sensor ${sensor} recebido: ${state}`);
+}
+
+function processUpdate(item) {
+    console.log('Atualização da agenda recebida:', item);
+}
+
+mqttService.mqttClient.on('message', (topic, message) => {
+    const data = JSON.parse(message.toString());
+
+    if (topic === 'Estado sensores') {
+        processSensorState(data);
+    } else if (topic === 'Agenda') {
+        processUpdate(data);
+    }
+})
