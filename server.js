571cd7dd05bccbe766240df2afe4559193653841
Victoria Martínez <94878673+pistashioz@users.noreply.github.com>
2024-11-28 17:21:02 +0000

    minor fix

--- server.js
+++ server.js
@@ -20,6 +20,6 @@ app.post('/publish', (req, res) => {
 })
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Servidor a correr na porta ${PORT}`);
-})
+app.listen(PORT, '0.0.0.0', () => { // o 0.0.0.0 é para que seja escutado em todas as interfaces de rede, o que significa que ele pode ser acessado por outros dispositivos que estejam ligado na mesma rede (tive que fazer isto porque o emulador não funciona no meu pc e tenho de testar tudo pelo meu tele)
+    console.log(`Servidor a correr na porta http://0.0.0.0:${PORT}`);
+});
