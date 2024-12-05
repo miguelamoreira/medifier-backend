8997dee4b5dd31792a88ddcafca447f3a5f89216
Victoria Martínez <94878673+pistashioz@users.noreply.github.com>
2024-12-05 21:23:54 +0000

    i dont remember

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,28 +5,40 @@ const bcrypt = require('bcryptjs');
 
 exports.login = async (req, res) => {
     const { email, password } = req.body;
-
-    const user = await User.findOne({ email });
-    if (!user) {
-        return res.status(404).json({
-            message: 'Utilizador não encontrado'
-        })
-    }
-
-    const check = bcrypt.compareSync(password.trim(), user.password);
+    try{
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(404).json({
+                message: 'Utilizador não encontrado'
+            })
+        }
+    
+        const check = bcrypt.compareSync(password.trim(), user.password);
+            
+        if (!check) {
+            return res.status(401).json({ 
+                message: 'Credenciais inválidas' 
+            });
+        }
         
-    if (!check) {
-        return res.status(401).json({ 
-            message: 'Credenciais inválidas' 
+        const token = jwt.sign({ userId: user._id, username: user.username, email: user.email }, process.env.JWT_SECRET, { expiresIn: '24h' });
+    
+        return res.status(200).json({ 
+            message: 'Login feito com sucesso', 
+            token,
+            user: {
+                id: user._id,
+                username: user.username,
+                email: user.email
+            }
+        });
+    } catch(error){
+        return res.status(500).json({
+            message: 'Erro interno do servidor',
+            error: error.message
         });
     }
-    
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '24h' });
 
-    return res.status(200).json({ 
-        message: 'Login feito com sucesso', 
-        token 
-    });
 };
 
 exports.register = async (req, res) => {
@@ -36,25 +48,32 @@ exports.register = async (req, res) => {
             message: 'Dados incompletos'
         })
     }
-
-    const existingUser = await User.findOne({ username });
-    if (existingUser) {
-        return res.status(400).json({
-            message: `O username ${username} já está a ser utilizado`
+    try{
+        const existingUser = await User.findOne({ username });
+        if (existingUser) {
+            return res.status(400).json({
+                message: `O username ${username} já está a ser utilizado`
+            })
+        }
+    
+        const existingEmail = await User.findOne({ email });
+        if (existingEmail) {
+            return res.status(400).json({
+                message: `O e-mail ${email} já está a ser utilizado`
+            })
+        }
+    
+        const hashedPassword = await bcrypt.hash(password, 10);
+    
+        const user = await User.create({ username, password: hashedPassword, email });
+        return res.status(201).json({
+            message: 'Utilizador criado com sucesso'
         })
     }
-
-    const existingEmail = await User.findOne({ email });
-    if (existingEmail) {
-        return res.status(400).json({
-            message: `O e-mail ${email} já está a ser utilizado`
-        })
+    catch(error){
+        return res.status(500).json({
+            message: 'Erro interno do servidor',
+            error: error.message
+        });
     }
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    const user = await User.create({ username, password: hashedPassword, email });
-    return res.status(201).json({
-        message: 'Utilizador criado com sucesso'
-    })
 }
